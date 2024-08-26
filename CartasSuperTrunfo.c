48994b4eb7d9d901e61587f6264cb05e5fbcadbd
Guilherme Rosa <anoobs@tutamail.com>
2024-08-26 13:37:57 +0000

    Input de string com espaços

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -13,30 +13,30 @@ int main(){
     int turisticSpots;
 
     printf("Digite o Estado da carta: ");
-    scanf("%c", &state);
+    scanf(" %c", &state);
     printf("Digite o código da carta: ");
-    fgets(cardCode, 3, stdin);
+    scanf(" %s", cardCode);
     printf("Digite o nome da cidade: ");
-    fgets(cityName, 20, stdin);
+    scanf(" %[^\n]s",cityName);
     printf("Digite a população: ");
-    scanf("%d", &cityPopulation);
+    scanf(" %d", &cityPopulation);
     printf("Digite o a área em km²: ");
-    scanf("%f", &cityArea);
+    scanf(" %f", &cityArea);
     printf("Digite o PIB: ");
-    scanf("%f", &pib);
+    scanf(" %f", &pib);
     printf("Digite o número de pontos turísticos: ");
-    scanf("%d", &turisticSpots);
+    scanf(" %d", &turisticSpots);
 
    
     printf("\n\n----------------------------------------\n");
-    printf("INFORÇÕES DA CARTA:\n");
+    printf("INFORÇÕES DA CARTA\n");
     printf("\t- Estado: %c\n", state);
-    printf("\t-Código da Carta: %s\n", cardCode);
-    printf("\t-Nome da Cidade: %s\n", cityName);
-    printf("\t-População: %d \n", cityPopulation);
-    printf("\t-Área: %f km\n²", cityArea);
-    printf("\t-PIB: %f\n", pib);
-    printf("\t-Número de Pontos Turísticos: %d\n", turisticSpots);
-    printf("\n\n----------------------------------------\n");
+    printf("\t- Código da Carta: %s\n", cardCode);
+    printf("\t- Nome da Cidade: %s\n", cityName);
+    printf("\t- População: %d \n", cityPopulation);
+    printf("\t- Área: %f km\n²", cityArea);
+    printf("\t- PIB: %f\n", pib);
+    printf("\t- Número de Pontos Turísticos: %d\n", turisticSpots);
+    printf("\n----------------------------------------\n");
 
 }
